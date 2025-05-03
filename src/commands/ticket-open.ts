1418c9a57b1096c11132efb962ec299ed0e52dd4
dani2318 <giacche.dani@gmail.com>
2025-05-04 00:45:30 +0200

    Added coin-flip, ticket-lock/unlock/open, urbandictionary search command. Modified .env.example to add needed ids for the ticketing system.

---
+++ src/commands/ticket-open.ts
@@ -0,0 +1,31 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder, TextChannel } from "discord.js";
+import dotenv from "dotenv"
+dotenv.config()
+
+const staffTicketChannel = process.env.DISCORD_STAFF_TICKET_CHANNEL
+
+if(!staffTicketChannel){
+  throw Error("You have not set the staff channel.")
+}
+
+const command = {
+	name: "ticketopen",
+	data: new SlashCommandBuilder().setName("ticketopen").setDescription("Open a ticket with the moderation team!")
+    .addStringOption(option =>
+        option.setName("message")
+            .setDescription("message to the staff")
+            .setRequired(true)),
+	async execute(interaction: ChatInputCommandInteraction) {
+    const channel: TextChannel = interaction.guild!.channels.cache.get(staffTicketChannel) as TextChannel;
+
+    if (channel.topic == "Channel Closed"){
+      await interaction.reply("🔴 Your ticket could not be open. please try again when we start taking ticket again.")
+      return
+    }
+
+    await channel.send("**User:** "+interaction.user.displayName+"\n**Timestamp:** "+new Date(interaction.createdTimestamp).toDateString()+"\n**Message:** \n"+ interaction.options.getString("message")!);
+    await interaction.reply("Your ticket has ben open. We will try to contact you as fast as we can!")
+	},
+};
+
+export = command;
