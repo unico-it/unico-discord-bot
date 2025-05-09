5b4336f9caaafb49cc23eb11ae4de568ecb9c9d7
albertoboccolini <albertoboccolini03@gmail.com>
2025-05-09 18:26:03 +0200

    Quick fix

--- src/commands/unlock-ticket-channel.ts
+++ src/commands/unlock-ticket-channel.ts
@@ -2,6 +2,12 @@ import { ChatInputCommandInteraction, SlashCommandBuilder, TextChannel, Permissi
 import dotenv from "dotenv";
 dotenv.config();
 
+const staffTicketChannel = process.env.DISCORD_STAFF_TICKET_CHANNEL;
+
+if (!staffTicketChannel) {
+	throw Error("You have not set the staff channel.");
+}
+
 const command = {
 	name: "unlock-ticket-channel",
 	data: new SlashCommandBuilder().setName("unlock-ticket-channel").setDescription("Unlock ticket channel."),
@@ -13,12 +19,6 @@ const command = {
 		if (!permission) {
 			interaction.reply("You do not have the needed permission to use this command!");
 			return;
-		}
-
-		const staffTicketChannel = process.env.DISCORD_STAFF_TICKET_CHANNEL;
-
-		if (!staffTicketChannel) {
-			throw Error("You have not set the staff channel.");
 		}
 
 		const channel: TextChannel = interaction.guild!.channels.cache.get(staffTicketChannel) as TextChannel;
