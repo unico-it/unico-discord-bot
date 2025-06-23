5864bb131e7837b9a34b3244abc24fba92964eff
dani2318 <70331791+dani2318@users.noreply.github.com>
2025-06-23 10:24:25 +0200

    Added clear command (#9)
    * Aggiunto comando clear
    
    * Runned lint:fix and implemented suggested corrections

---
+++ src/commands/clear.ts
@@ -0,0 +1,31 @@
+import type { ChatInputCommandInteraction, TextChannel } from 'discord.js';
+import { ChannelType, MessageFlags, SlashCommandBuilder } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const command = {
+  name: 'clear',
+  data: new SlashCommandBuilder()
+    .setName('clear')
+    .setDescription('Clears the last 100 messages from a channel. This improves readability in channels where the user sends commands to bots, making them readable by all users.')
+    .addChannelOption((input) => input.setName('channel').addChannelTypes(ChannelType.GuildText).setDescription('Name of the channel to clear').setRequired(true)),
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    await interaction.deferReply({
+      flags: MessageFlags.Ephemeral,
+    });
+    const channelId = interaction.options.data[0]?.channel?.id;
+
+    const channel = interaction.guild?.channels.cache.get(channelId!) as TextChannel;
+
+    const messages = (await channel.messages.fetch({ limit: 100 })).filter(m => !m.pinned);
+    await channel.bulkDelete(messages, true);
+
+
+    await interaction.editReply({
+      content: `🧹 Done! Messages deleted from #${channel.name}.`,
+    });
+  }
+};
+
+export = command;
